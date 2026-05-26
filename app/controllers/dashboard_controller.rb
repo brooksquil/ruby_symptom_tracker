437cc7cfd0f6cd933e7824b3a9c5bd961bdeb3fd
brooksquil <bwittenberg.developer@gmail.com>
2026-05-25 22:14:33 -0500

    auth complete

---
+++ app/controllers/dashboard_controller.rb
@@ -0,0 +1,9 @@
+class DashboardController < ApplicationController
+  
+  def show
+    # @user = current_user
+    # @symptoms = current_user.symptoms
+    # @diagnoses = current_user.diagnoses
+    # @symptom_entries = current_user.symptom_entries
+  end
+end
