23040478173f9a76f5b46ceb1714b0169b69115a
brooksquil <bwittenberg.developer@gmail.com>
2026-05-26 00:46:55 -0500

    fix due to failed tests

--- test/controllers/dashboard_controller_test.rb
+++ test/controllers/dashboard_controller_test.rb
@@ -1,8 +1,17 @@
 require "test_helper"
 
 class DashboardControllerTest < ActionDispatch::IntegrationTest
-  test "should get show" do
+  test "redirects unauthenticated users to sign in" do
     get dashboard_show_url
+
+    assert_redirected_to new_session_path
+  end
+
+  test "authenticated user can see dashboard" do
+    sign_in_as(users(:one))
+
+    get dashboard_show_url
+
     assert_response :success
   end
 end
